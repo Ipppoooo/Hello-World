2a47a921865d0fa40d2098d5028d8f49d198a32f
Ipppoooo <153452070+Ipppoooo@users.noreply.github.com>
2023-12-10 21:12:40 -0800

    Add files via upload

---
+++ NameInput.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+
+public class NameInput {
+    //input function
+    public static String inputName(){
+    String name = JOptionPane.showInputDialog("Enter Your Name:");
+        if(name == null){
+            System.out.println("You Cancelled the Input");
+        }
+        else
+    {
+       System.err.println(name);
+    }
+        return name;
+       
+    }
+    // Function to recall a person's name
+    public static void recallName(String name) {
+        System.out.println("Hello, " + name + "! Nice to meet you.");
+    }
+
+    
+    public static void main(String[] args) {
+
+               String name=inputName();
+                recallName(name);
+    }
+
+
+    @Override
+    public String toString() {
+        return "inputName []";
+    }
+}
